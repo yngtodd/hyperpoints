2f66c89ec76617ee3e840c78f2a612d97633e259
yngtodd <young.todd.mk@gmail.com>
2018-12-13 13:21:14 -0500

    Updating reward gathering

--- hyperpoints/utils/utils.py
+++ hyperpoints/utils/utils.py
@@ -17,7 +17,17 @@ def gather_rewards(path: str, savefile: str=None) -> pd.DataFrame:
     p = Path(path)
     results_paths = [x for x in p.iterdir() if x.is_dir()]
     results_paths = [x.joinpath('progress.csv') for x in results_paths]
-    results = [pd.read_csv(x) for x in results_paths]
+
+    results = []
+    for i in results_paths:
+        try:
+            res = pd.read_csv(i)
+            if len(res) > 1000:
+                results.append(res)
+        except Exception:
+            pass
+
+    print(f'Number of results: {len(results)}')
     rewards = [np.array(x['PMM:episode_rewards']) for x in results]
     rewards = pd.concat([pd.Series(x) for x in rewards], axis=1)
 
